debcdc7dcd445d2a309194e6bb494a94d78644ed
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-05-29 23:37:05 +0530

    Defining a route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+  req.params;
+  res.status(200).json(req.params);
 });
 
 module.exports = app;
